f96262b56aeddb9d083000577f3a20a49de03516
j0hnton <afrikiddo@gmail.com>
2019-04-30 15:49:09 +0300

    Adding tests

--- src/test/java/HeroSalonTest.java
+++ src/test/java/HeroSalonTest.java
@@ -17,4 +17,20 @@ public class HeroSalonTest {
         }
     }
 
+    @Test
+    public void Client_instantiatesCorrectly_true() {
+        Client myClient = new Client("name", "gender", "cname");
+        assertEquals(true, myClient instanceof Client);
+    }
+    @Test
+    public void Stylist_instantiatesCorrectly_true() {
+        Stylist myStylist = new Stylist("name","gender");
+        assertEquals(true, myStylist instanceof Stylist);
+    }
+    @Test
+    public void App_instantiatesCorrectly_true() {
+        App myApp = new App();
+        assertEquals(true, myApp instanceof App);
+    }
+
 }
